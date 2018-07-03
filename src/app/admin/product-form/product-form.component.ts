2c92c9b56f6a154c344a1008d217263485b4c4fe
Drake Fish <drakefish@drakes-mbp.attlocal.net>
2018-07-02 22:06:52 -0500

    completed adding a product, updating a product and deleting a product from the database

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-form',
@@ -9,15 +12,34 @@ import { ProductService } from '../../product.service';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  id;
+  product = {};
   constructor(
-    categoryService: CategoryService,
-    private productService: ProductService
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.categories$ = categoryService.getCategories();
+
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.productService
+        .get(this.id)
+        .take(1)
+        .subscribe(p => (this.product = p.payload.val()));
+    }
   }
   save(product) {
     //make sure that the products in the right shape.
-    this.productService.create(product);
+    if (this.id) this.productService.update(this.id, product);
+    else this.productService.create(product);
+    this.router.navigate(['/admin/products']);
+  }
+  delete() {
+    if (!confirm('Are you sure you want to delete this product?')) return;
+    this.productService.delete(this.id);
+    this.router.navigate(['/admin/products']);
   }
 
   ngOnInit() {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,15 +74,20 @@ import { ProductService } from './product.service';
         component: OrderSuccessComponent,
         canActivate: [AuthGuard]
       },
+      {
+        path: 'admin/products/new',
+        component: ProductFormComponent,
+        canActivate: [AuthGuard, AdminAuthGuard]
+      },
+      {
+        path: 'admin/products/:id',
+        component: ProductFormComponent,
+        canActivate: [AuthGuard, AdminAuthGuard]
+      },
 
       {
         path: 'admin/products',
         component: AdminProductsComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
-      },
-      {
-        path: 'admin/products/new',
-        component: ProductFormComponent,
         canActivate: [AuthGuard, AdminAuthGuard]
       },
       {
