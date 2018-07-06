ec6a63d8ab4740fd472b938489863fd21e4dc4b5
Drake Fish <drakefish@drakes-mbp.attlocal.net>
2018-07-05 20:55:44 -0500

    completed home page display, and category filtering

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  categories$;
+  category: string;
+  constructor(
+    productService: ProductService,
+    categoryService: CategoryService,
+    route: ActivatedRoute
+  ) {
+    this.products$ = productService.getAll().subscribe(products => {
+      this.products = products;
 
-  constructor() { }
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
 
-  ngOnInit() {
+        this.filteredProducts = this.category
+          ? this.products.filter(p => p.category === this.category)
+          : this.products;
+      });
+    });
+
+    this.categories$ = categoryService.getCategories();
   }
-
 }
